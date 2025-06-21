2c961db358fdd4e4d74d28384fa4990f7f19eeed
kvxd <redslyice@outlook.de>
2025-06-21 19:01:39 +0200

    fix: Remove ktor plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.plugin.serialization)
 
     id("io.deepmedia.tools.deployer") version "0.18.0"
